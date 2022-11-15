f5d48996b164b7bd53eb45cf368f67efae7e30f4
nlaobappha <114454714+nlaobappha@users.noreply.github.com>
2022-11-14 23:28:44 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Nathanon Laobappha', // Title for your website.
     tagline: 'Creative, Determined, Open-minded.',
-    profilePhoto: 'img/profile.png',
+    profilePhoto: 'fruitInspector.png',
     url: 'https://nlaobappha.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
